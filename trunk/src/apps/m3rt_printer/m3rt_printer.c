590e4a2d724a20355f4cf3ac863bc50b9a1fb923
meka <meka@1168f755-9803-407b-a38c-2651d5e71887>
2011-09-24 02:44:35 +0000

    git-svn-id: file:///home/svn/m3meka@17 1168f755-9803-407b-a38c-2651d5e71887

--- trunk/src/apps/m3rt_printer/m3rt_printer.c
+++ trunk/src/apps/m3rt_printer/m3rt_printer.c
@@ -36,9 +36,10 @@ along with M3.  If not, see <http://www.gnu.org/licenses/>.
 #include "pthread.h"
 #include "m3/hardware/m3meka_ec_def.h"
 #include "m3rt/base/m3ec_def.h"
-#include "m3rt/base/m3ec_pdo_v0_def.h"
-#include "m3rt/base/m3ec_pdo_v1_def.h"
-#include "m3rt/base/m3ec_pdo_v2_def.h"
+#include "m3/hardware/m3ec_pdo_v0_def.h"
+#include "m3/hardware/m3ec_pdo_v1_def.h"
+#include "m3/hardware/m3ec_pdo_v2_def.h"
+#include "m3/hardware/m3ec_pdo_v3_def.h"
 #include "m3rt/base/m3rt_def.h"
 #include "stdio.h"
 
