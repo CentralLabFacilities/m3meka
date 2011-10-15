593a97c3beba5ab4d6ebf819117675d17ae3fa2a
meka <meka@1168f755-9803-407b-a38c-2651d5e71887>
2011-10-15 02:36:19 +0000

    Working version, added the hex files
    git-svn-id: file:///home/svn/m3meka@83 1168f755-9803-407b-a38c-2651d5e71887

--- trunk/src/m3dsp/boards/MAX2/src/bldc.h
+++ trunk/src/m3dsp/boards/MAX2/src/bldc.h
@@ -28,8 +28,12 @@ void setup_bldc(void);
 void set_bldc_dir(unsigned int fwd);
 extern unsigned int bldc_hall_val;
 
-#define BLDC_HALL_2 PORTBbits.RB4		//RB4	INPUT	PIN33	RP4/CN1 (HALL1) 
-#define BLDC_HALL_1 PORTBbits.RB9		//RB9	INPUT	PIN1	RP9/CN21 (HALL2)
+//#define BLDC_HALL_2 PORTBbits.RB4		//RB4	INPUT	PIN33	RP4/CN1 (HALL1) 
+//#define BLDC_HALL_1 PORTBbits.RB9		//RB9	INPUT	PIN1	RP9/CN21 (HALL2)
+//#define BLDC_HALL_3 PORTBbits.RB8		//RB8	INPUT	PIN44	RP8/CN22 (HALL3)
+
+#define BLDC_HALL_1 PORTBbits.RB4		//RB4	INPUT	PIN33	RP4/CN1 (HALL1) 
+#define BLDC_HALL_2 PORTBbits.RB9		//RB9	INPUT	PIN1	RP9/CN21 (HALL2)
 #define BLDC_HALL_3 PORTBbits.RB8		//RB8	INPUT	PIN44	RP8/CN22 (HALL3)
 
 #define BLDC_HALL_STATE (BLDC_HALL_3<<2)|(BLDC_HALL_2<<1)|BLDC_HALL_1
