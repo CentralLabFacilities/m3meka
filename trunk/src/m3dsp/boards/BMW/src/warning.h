b0782a51221fc376d8340c0533414d045f96ea67
meka <meka@1168f755-9803-407b-a38c-2651d5e71887>
2011-11-01 18:08:53 +0000

    git-svn-id: file:///home/svn/m3meka@124 1168f755-9803-407b-a38c-2651d5e71887

---
+++ trunk/src/m3dsp/boards/BMW/src/warning.h
@@ -0,0 +1,11 @@
+#ifndef DEF_WARNING_H	
+#define DEF_WARNING_H	
+
+//Compilation warnings:
+#ifdef BMW_0_5_A2R4
+	#warning "Project: BMW_0_5_A2R4"
+#else
+	#warning "How do you expect it to work if you don't specify what robot?"
+#endif
+
+#endif
