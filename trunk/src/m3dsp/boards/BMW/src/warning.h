220c54c93c0dd4843d16d287ce22dbb63b323b2c
jfduval <jfduval@1168f755-9803-407b-a38c-2651d5e71887>
2011-12-13 23:51:01 +0000

    git-svn-id: file:///home/svn/m3meka@134 1168f755-9803-407b-a38c-2651d5e71887

--- trunk/src/m3dsp/boards/BMW/src/warning.h
+++ trunk/src/m3dsp/boards/BMW/src/warning.h
@@ -1,6 +1,9 @@
 #ifndef DEF_WARNING_H	
 #define DEF_WARNING_H	
 
+#warning "You should try the new 4Q table developped for LCM"
+#warning "Sorry, had no time to integrate/test it in this project. -JF"
+
 //Compilation warnings:
 #ifdef BMW_0_5_A2R4
 	#warning "Project: BMW_0_5_A2R4"
