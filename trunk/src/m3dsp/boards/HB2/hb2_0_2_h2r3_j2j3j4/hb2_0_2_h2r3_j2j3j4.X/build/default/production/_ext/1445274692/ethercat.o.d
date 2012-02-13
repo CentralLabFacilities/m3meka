0eea384b53c7ce03a7f11c922c61b4779c767268
meka <meka@1168f755-9803-407b-a38c-2651d5e71887>
2012-02-13 20:35:27 +0000

    git-svn-id: file:///home/svn/m3meka@146 1168f755-9803-407b-a38c-2651d5e71887

---
+++ trunk/src/m3dsp/boards/HB2/hb2_0_2_h2r3_j2j3j4/hb2_0_2_h2r3_j2j3j4.X/build/default/production/_ext/1445274692/ethercat.o.d
@@ -0,0 +1,10 @@
+build/default/production/_ext/1445274692/ethercat.o:  \
+ ../../src/ethercat.c ../../src/setup.h \
+  ../../src/../../../../m3/hardware/m3ec_def.h \
+  ../../src/../../../../m3/hardware/m3ec_pdo_v1_def.h \
+  ../../src/../../../../m3/hardware/m3ec_pdo_v2_def.h \
+  ../../src/../../../../m3/hardware/m3ec_pdo_v0_def.h ../../src/dio.h \
+  ../../src/ethercat.h ../../src/timer3.h ../../src/encoder_ma3.h \
+  ../../src/pwm.h ../../src/control.h ../../src/adc.h \
+  ../../src/ethercat_def.h ../../src/ethercat_slave_fsm.h \
+  ../../src/ethercat_hw.h ../../src/ethercat_esc.h
