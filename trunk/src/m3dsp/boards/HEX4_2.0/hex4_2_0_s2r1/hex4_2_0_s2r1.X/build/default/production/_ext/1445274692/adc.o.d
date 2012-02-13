2a1c99ba2a6a6fdb7771fd288e9be451407fad28
meka <meka@1168f755-9803-407b-a38c-2651d5e71887>
2012-02-13 22:54:03 +0000

    git-svn-id: file:///home/svn/m3meka@153 1168f755-9803-407b-a38c-2651d5e71887

--- trunk/src/m3dsp/boards/HEX4_2.0/hex4_2_0_s2r1/hex4_2_0_s2r1.X/build/default/production/_ext/1445274692/adc.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/_ext/1445274692/adc.o: ../../src/adc.c \
-  ../../src/setup.h ../../src/inttypes.h \
-  ../../src/../../../../m3/hardware/m3ec_pdo_v3_def.h \
-  ../../src/../../../../m3/hardware/m3ec_pdo_v1_def.h ../../src/dio.h \
-  ../../src/ethercat.h ../../src/encoder_vertx.h ../../src/pwm.h \
-  ../../src/control.h ../../src/adc.h ../../src/current.h \
-  ../../src/timer3.h ../../src/bldc.h
--- trunk/src/m3dsp/boards/HEX4_2.0/hex4_2_0_s2r1/hex4_2_0_s2r1.X/build/default/production/_ext/1445274692/timer3.o.d
+++
@@ -1,1 +0,0 @@
-build/default/production/_ext/1445274692/timer3.o: ../../src/timer3.c
