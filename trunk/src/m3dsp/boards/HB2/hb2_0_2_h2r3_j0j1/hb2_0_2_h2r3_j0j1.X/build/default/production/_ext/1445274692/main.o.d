2f59625aa9a0d5dd676b133c1b2fc27b26f03867
meka <meka@1168f755-9803-407b-a38c-2651d5e71887>
2012-02-13 22:55:55 +0000

    git-svn-id: file:///home/svn/m3meka@158 1168f755-9803-407b-a38c-2651d5e71887

--- trunk/src/m3dsp/boards/HB2/hb2_0_2_h2r3_j0j1/hb2_0_2_h2r3_j0j1.X/build/default/production/_ext/1445274692/main.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/_ext/1445274692/main.o: ../../src/main.c \
-  ../../src/warning.h ../../src/setup.h \
-  ../../src/../../../../m3/hardware/m3ec_def.h \
-  ../../src/../../../../m3/hardware/m3ec_pdo_v1_def.h \
-  ../../src/../../../../m3/hardware/m3ec_pdo_v2_def.h \
-  ../../src/../../../../m3/hardware/m3ec_pdo_v0_def.h ../../src/dio.h \
-  ../../src/ethercat.h ../../src/timer3.h ../../src/encoder_ma3.h \
-  ../../src/pwm.h ../../src/control.h ../../src/adc.h
