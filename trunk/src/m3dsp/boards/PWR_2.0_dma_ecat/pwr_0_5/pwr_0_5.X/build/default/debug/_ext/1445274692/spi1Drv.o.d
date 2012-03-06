c368c86ad6ba97eaa74a787cf1987f9893f07a5d
meka <meka@1168f755-9803-407b-a38c-2651d5e71887>
2012-03-06 21:37:51 +0000

    git-svn-id: file:///home/svn/m3meka@163 1168f755-9803-407b-a38c-2651d5e71887

--- trunk/src/m3dsp/boards/PWR_2.0_dma_ecat/pwr_0_5/pwr_0_5.X/build/default/debug/_ext/1445274692/spi1Drv.o.d
+++
@@ -1,8 +0,0 @@
-build/default/debug/_ext/1445274692/spi1Drv.o: ../../src/spi1Drv.c \
-  ../../src/setup.h ../../src/../../../../m3/hardware/m3ec_def.h \
-  ../../src/../../../../m3/hardware/m3ec_pdo_v1_def.h \
-  ../../src/../../../../m3/hardware/m3ec_pdo_v2_def.h \
-  ../../src/../../../../m3/hardware/m3ec_pdo_v0_def.h ../../src/dio.h \
-  ../../src/ethercat.h ../../src/timer3.h ../../src/control.h \
-  ../../src/adc.h ../../src/adc_spi.h ../../src/ethercat_hw.h \
-  ../../src/ethercat_def.h ../../src/ethercat_esc.h
