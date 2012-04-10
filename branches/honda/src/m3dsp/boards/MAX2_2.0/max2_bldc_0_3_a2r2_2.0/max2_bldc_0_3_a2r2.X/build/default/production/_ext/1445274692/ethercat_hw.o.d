4334c1b5bc9b34ade70b11b591be5157df6f4853
meka <meka@1168f755-9803-407b-a38c-2651d5e71887>
2012-04-10 19:55:34 +0000

    git-svn-id: file:///home/svn/m3meka@185 1168f755-9803-407b-a38c-2651d5e71887

---
+++ branches/honda/src/m3dsp/boards/MAX2_2.0/max2_bldc_0_3_a2r2_2.0/max2_bldc_0_3_a2r2.X/build/default/production/_ext/1445274692/ethercat_hw.o.d
@@ -0,0 +1,11 @@
+build/default/production/_ext/1445274692/ethercat_hw.o:  \
+ ../../src/ethercat_hw.c ../../src/ethercat_hw.h ../../src/ethercat_def.h \
+  ../../src/ethercat_esc.h ../../src/ethercat.h ../../src/setup.h \
+  ../../src/inttypes.h \
+  ../../src/../../../../m3/hardware/m3ec_pdo_v1_def.h \
+  ../../src/../../../../m3/hardware/m3ec_pdo_v3_def.h ../../src/dio.h \
+  ../../src/timer1.h ../../src/encoder_vertx.h ../../src/pwm.h \
+  ../../src/control.h ../../src/adc.h ../../src/current.h \
+  ../../src/timer3.h ../../src/bldc.h ../../src/brake.h \
+  ../../src/adc_spi.h ../../src/ethercat_slave_fsm.h \
+  ../../src/ethercat_appl.h
---
+++ branches/honda/src/m3dsp/boards/MAX2_2.0/max2_bldc_0_3_a2r2_2.0/max2_bldc_0_3_a2r2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=max2_bldc_0_3_a2r2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/max2_bldc_0_3_a2r2.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=max2bldc03a2r2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/max2bldc03a2r2.x.tar
