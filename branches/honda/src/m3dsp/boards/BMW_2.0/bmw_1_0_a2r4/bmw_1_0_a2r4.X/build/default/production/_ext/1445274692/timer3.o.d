242f7b1f52755feef1d985e9823aca3177c02320
rkelbs <rkelbs@1168f755-9803-407b-a38c-2651d5e71887>
2012-04-11 02:59:59 +0000

    git-svn-id: file:///home/svn/m3meka@191 1168f755-9803-407b-a38c-2651d5e71887

--- branches/honda/src/m3dsp/boards/BMW_2.0/bmw_1_0_a2r4/bmw_1_0_a2r4.X/build/default/production/_ext/1445274692/timer3.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/_ext/1445274692/timer3.o: ../../src/timer3.c \
-  ../../src/timer3.h ../../src/setup.h ../../src/inttypes.h \
-  ../../src/../../../../m3/hardware/m3ec_pdo_v3_def.h \
-  ../../src/../../../../m3/hardware/m3ec_pdo_v1_def.h ../../src/dio.h \
-  ../../src/ethercat.h ../../src/encoder_vertx.h ../../src/pwm.h \
-  ../../src/control.h ../../src/adc.h ../../src/current.h \
-  ../../src/bldc.h
