2db493c85e09739d1354e12b791f65fc77cf5cfb
meka <meka@1168f755-9803-407b-a38c-2651d5e71887>
2012-06-13 19:58:40 +0000

    deleting abandoned dsp folders
    git-svn-id: file:///home/svn/m3meka@264 1168f755-9803-407b-a38c-2651d5e71887

--- trunk/src/m3dsp/max2_bldc_a2r4/m3_controller_max2_bldc_a2r4.X/build/default/production/_ext/1472/ethercat_hw.o.d
+++
@@ -1,6 +0,0 @@
-build/default/production/_ext/1472/ethercat_hw.o: ../ethercat_hw.c \
-  ../ethercat_hw.h ../ethercat_def.h ../ethercat_esc.h ../ethercat.h \
-  ../setup.h ../inttypes.h ../../../m3/hardware/m3ec_pdo_v3_def.h \
-  ../dio.h ../timer1.h ../encoder_vertx.h ../pwm.h ../control.h ../adc.h \
-  ../current.h ../timer3.h ../bldc.h ../ethercat_slave_fsm.h \
-  ../ethercat_appl.h
--- trunk/src/m3dsp/max2_bldc_a2r4/m3_controller_max2_bldc_a2r4.X/build/default/production/_ext/1472/timer3.o.d
+++
@@ -1,1 +0,0 @@
-build/default/production/_ext/1472/timer3.o: ../timer3.c
