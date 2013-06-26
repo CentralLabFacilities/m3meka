3a5f8ef2010183c9bb9d2663545e22c25be58810
rkelbs <rkelbs@1168f755-9803-407b-a38c-2651d5e71887>
2013-06-26 23:41:10 +0000

    git-svn-id: file:///home/svn/m3meka@470 1168f755-9803-407b-a38c-2651d5e71887

---
+++ trunk/src/m3dsp/boards/ELMO_2.2_Z/max2_bldc_2_0_a2r4/max2_bldc_2_0_a2r4/build/default/production/_ext/1445274692/control.o.d
@@ -0,0 +1,11 @@
+build/default/production/_ext/1445274692/control.o: ../../src/control.c \
+  ../../src/setup.h ../../src/inttypes.h \
+  ../../src/../../../../m3/hardware/m3ec_pdo_v1_def.h \
+  ../../src/../../../../m3/hardware/m3ec_pdo_v4_def.h \
+  ../../src/../../../../m3/hardware/m3ec_pdo_v0_def.h \
+  ../../src/../../../../m3/hardware/m3ec_pdo_v2_def.h ../../src/dio.h \
+  ../../src/ethercat.h ../../src/timer1.h ../../src/encoder_vertx.h \
+  ../../src/encoder_qei.h ../../src/pwm.h ../../src/control.h \
+  ../../src/dac.h ../../src/adc.h ../../src/current.h ../../src/timer3.h \
+  ../../src/bldc.h ../../src/brake.h ../../src/adc_spi.h \
+  ../../src/state_machine.h ../../src/temperature_model.h
