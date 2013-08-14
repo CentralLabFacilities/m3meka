2384e3be854f1c3bedc4472513b11895b618bf62
rkelbs <rkelbs@1168f755-9803-407b-a38c-2651d5e71887>
2013-08-14 20:41:47 +0000

    git-svn-id: file:///home/svn/m3meka@477 1168f755-9803-407b-a38c-2651d5e71887

--- trunk/src/m3dsp/boards/MAX2_2.1_qei/src/adc.c
+++ trunk/src/m3dsp/boards/MAX2_2.1_qei/src/adc.c
@@ -1,26 +1,21 @@
- /*
-Copyright (c) 2010, Meka Robotics
-All rights reserved.
-http://mekabot.com
+/* 
+M3 -- Meka Robotics Real-Time Control System
+Copyright (c) 2010 Meka Robotics
+Author: edsinger@mekabot.com (Aaron Edsinger)
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted. 
+M3 is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
+M3 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
 
-THIS SOFTWARE IS PROVIDED BY THE Copyright HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-Copyright OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
+You should have received a copy of the GNU Lesser General Public License
+along with M3.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #ifdef USE_ADC
 
 #include "p33Fxxxx.h"
