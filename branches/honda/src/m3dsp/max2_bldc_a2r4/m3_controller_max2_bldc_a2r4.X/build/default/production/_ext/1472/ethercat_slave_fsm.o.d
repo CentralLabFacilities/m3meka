9339bf5d090909bbe43491ec0405fff1a6a4d65d
semeyerz <semeyerz@TechFak.Uni-Bielefeld.DE>
2014-11-25 09:56:46 +0100

    restructure mirror branch

--- branches/honda/src/m3dsp/max2_bldc_a2r4/m3_controller_max2_bldc_a2r4.X/build/default/production/_ext/1472/ethercat_slave_fsm.o.d
+++
@@ -1,6 +0,0 @@
-build/default/production/_ext/1472/ethercat_slave_fsm.o:  \
- ../ethercat_slave_fsm.c ../ethercat_slave_fsm.h ../ethercat_hw.h \
-  ../ethercat_def.h ../ethercat_esc.h ../ethercat_appl.h ../setup.h \
-  ../inttypes.h ../../../m3/hardware/m3ec_pdo_v3_def.h ../dio.h \
-  ../ethercat.h ../timer1.h ../encoder_vertx.h ../pwm.h ../control.h \
-  ../adc.h ../current.h ../timer3.h ../bldc.h
--- branches/honda/src/m3dsp/max2_bldc_a2r4/m3_controller_max2_bldc_a2r4.X/build/default/production/_ext/1472/setup.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1472/setup.o: ../setup.c ../setup.h \
-  ../inttypes.h ../../../m3/hardware/m3ec_pdo_v3_def.h ../dio.h \
-  ../ethercat.h ../timer1.h ../encoder_vertx.h ../pwm.h ../control.h \
-  ../adc.h ../current.h ../timer3.h ../bldc.h
